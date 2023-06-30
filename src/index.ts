a24979f42e49919cfdbe5e33e2d022c95a6233dc
jrobsontull <6155533+jrobsontull@users.noreply.github.com>
2023-06-30 22:59:21 +0200

    fix: index exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+export { copyDocument, copyPage, copyPages } from './pdf-modify/copy';
 export {
   createDocument,
   loadDocument,
+  loadLocalDocument,
+  loadFromBytes,
   documentToBlobUrl,
 } from './pdf-modify/create';
+export { insertPage, insertPages } from './pdf-modify/insert';
+export { mergeDocuments, mergePages, mergeFiles } from './pdf-modify/merge';
+export { getMeta, setMeta, resetMeta } from './pdf-modify/meta';
 export { rotatePage, rotatePages, rotateDocument } from './pdf-modify/rotate';
-export { mergeDocuments, mergePages } from './pdf-modify/merge';
